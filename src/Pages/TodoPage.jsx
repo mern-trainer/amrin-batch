64816214d44f61dce707d849557bf5f32781858f
VISHNU M K <futuralabs.mern@gmail.com>
2024-10-28 12:34:01 +0530

    todo status update

--- src/Pages/TodoPage.jsx
+++ src/Pages/TodoPage.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
-import { FaTrash } from "react-icons/fa6"
+import { FaCheck, FaCheckCircle } from "react-icons/fa"
+import { FaCircleXmark, FaTrash, FaXmark } from "react-icons/fa6"
+import { v4 } from "uuid"
 
 const TodoPage = () => { // todo application => task manager
     
@@ -10,12 +12,40 @@ const TodoPage = () => { // todo application => task manager
         if (todo == "") {
             return alert("Task is required!")
         }
-        setTodoList([todo, ...todoList])
+        const uniqueId = v4()
+        const taskObject = {
+            id: uniqueId,
+            title: todo,
+            completed: false,
+            createdAt: new Date().toLocaleString("en-IN").toUpperCase(),
+            updatedAt: new Date().toLocaleString("en-IN").toUpperCase(),
+        }
+        setTodoList([taskObject, ...todoList])
         setTodo("")
     }
 
-    const handleRemove = (idx) => {
-        const res = todoList.filter((_, index) => index != idx)
+    const handleRemove = (removeId) => { 
+        const res = todoList.filter((item) => item.id !== removeId)
+        setTodoList(res)
+    }
+
+    const handleStatusUpdate = (updateId) => {
+        const res = todoList.map(item => {
+            if (item.id == updateId) {
+                let status;
+                if (item.completed) {
+                    status = false
+                } else {
+                    status = true
+                }
+                return {
+                    ...item,
+                    completed: status,
+                    updatedAt: new Date().toLocaleString("en-IN").toUpperCase(),
+                }
+            }
+            return item
+        })
         setTodoList(res)
     }
 
@@ -26,14 +56,17 @@ const TodoPage = () => { // todo application => task manager
         </div>
         <div className="w-50 mt-3 d-flex flex-column gap-2">
             {
-                todoList.map((item, index) => {
+                todoList.map((item) => {
                     // key => unique identifier
-                    return <div key={index} className="p-2 bg-secondary  d-flex justify-content-between text-light w-100">
-                        <div style={{width: "90%"}} className="text-truncate">
-                            {item}
+                    return <div key={item.id} className="p-2 bg-light  d-flex justify-content-between text-dark w-100">
+                        <div style={{width: "90%"}} className="text-truncate d-flex gap-2 align-items-center">
+                            {item.completed && <div><FaCheckCircle color="green" /></div>}
+                            {!item.completed && <div><FaCircleXmark color="red"/></div>}
+                            <div>{item.title}</div>
                         </div>
                         <div style={{width: "10%"}} className="text-center">
-                            <FaTrash cursor={"pointer"} onClick={() => handleRemove(index)}/>
+                            <FaTrash cursor={"pointer"} onClick={() => handleRemove(item.id)}/>
+                            <FaCheck cursor={"pointer"} onClick={() => handleStatusUpdate(item.id)}/>
                         </div>
                     </div>
                 })
