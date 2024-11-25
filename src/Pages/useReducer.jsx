b01ecb663e99ec0f7c3b064b3189dfdab66cb952
VISHNU M K <futuralabs.mern@gmail.com>
2024-11-25 12:33:02 +0530

    password generator with use reducer

--- src/Pages/useReducer.jsx
+++ src/Pages/useReducer.jsx
@@ -1,54 +1,54 @@
 import React, { useReducer } from 'react'
+import { FaTrash } from 'react-icons/fa'
+
+// Random password generator
+
+const strings = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '[', ']', '{', '}', '|', ';', ':', '\'', ',', '.', '<', '>', '/', '?', '`', '~'
+]
 
 const reducer = (state, action) => {
-    if (action.type === "counter/increment1") {
-        return { ...state, counter1: state.counter1 + action.payload.number }
+    if (action.type === "passwords/create") {
+        return [action.payload, ...state]
     }
-    if (action.type === "counter/increment2") {
-        return { ...state, counter2: state.counter2 + action.payload.number }
-    }
-    if (action.type === "counter/decrement1") {
-        return { ...state, counter1: state.counter1 - action.payload.number }
-    }
-    if (action.type === "counter/decrement2") {
-        return { ...state, counter2: state.counter2 - action.payload.number }
+    if (action.type === "passwords/remove") {
+        return state.filter((_, index) => index !== action.payload.index)
     }
 }
 
 const UseReducer = () => {
-    
-    const [counter, dispatch] = useReducer(reducer, {
-        counter1: 0,
-        counter2: 0
-    })
 
-    const handleUpdate = (type, operator) => {
-        const obj = { payload: { number: 100 } }
-        if (type === 1 && operator === "+") {
-            obj.type = "counter/increment1"
+    const [passwords, dispatch] = useReducer(reducer, [])
+
+    const generatePassword = () => {
+        let i = 0
+        let password = ""
+        while (i < 16) {
+            const randomIndex = Math.floor(Math.random() * strings.length)
+            const element = strings[randomIndex]
+            password += element
+            i++
         }
-        if (type === 2 && operator === "+") {
-            obj.type = "counter/increment2"
-        }
-        if (type === 1 && operator === "-") {
-            obj.type = "counter/decrement1"
-        }
-        if (type === 2 && operator === "-") {
-            obj.type = "counter/decrement2"
-        }
-        dispatch(obj)
+        dispatch({ type: "passwords/create", payload: password });
+    }
+
+    const handleRemove = idx => {
+        dispatch({ type: "passwords/remove", payload: { index: idx } });
     }
 
     return <div className="d-flex flex-column align-items-center mt-4 gap-3">
-        <div>Counter 1: {counter.counter1}</div>
-        <div>Counter 2: {counter.counter2}</div>
-        <div className="d-flex gap-3">
-            <button className="btn btn-success rounded-0" onClick={() => handleUpdate(1, "+")}>Increment 1</button>
-            <button className="btn btn-danger rounded-0" onClick={() => handleUpdate(1, "-")}>Decrement 1</button>
-        </div>
-        <div className="d-flex gap-3">
-            <button className="btn btn-success rounded-0" onClick={() => handleUpdate(2, "+")}>Increment 2</button>
-            <button className="btn btn-danger rounded-0" onClick={() => handleUpdate(2, "-")}>Decrement 2</button>
+        <button className="btn btn-success rounded" onClick={generatePassword}>Generate Password</button>
+        <h3>Password History</h3>
+        <div className='w-50 d-flex flex-column gap-2'>
+            {
+                passwords.map((pass, idx) => {
+                    return <div key={idx} className="d-flex bg-light p-2 justify-content-between">
+                        <div>{pass}</div>
+                        <FaTrash cursor={"pointer"} color='red' onClick={() => handleRemove(idx)}/>
+                    </div>
+                })
+            }
         </div>
     </div>
 }
